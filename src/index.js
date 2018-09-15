5529db4eeb317a3ee4cc52a647b7efe44b9f5bfa
ilya <ilya@ilyas-MBP.Dlink>
2018-09-15 11:55:31 +0300

    no solution

--- src/index.js
+++ src/index.js
@@ -3,5 +3,26 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+
+  let countArr = [];
+
+  for (let i = 0; i < preferences.length; i+=3) {
+
+    if ( preferences[i] - preferences[i+1] == -1 && preferences[i+1] - preferences[i+2] == 2 ) {
+
+      let sum = preferences[i] + preferences[i+1] + preferences[i+2];
+
+      if ( !inArray(countArr, sum)) {
+        countArr.push(sum);
+      }
+    }
+  }
+  return countArr.length;
 };
+
+function inArray(arr, item) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == item) return true;
+  }
+  return false;
+}
